051b5e77ab1bcbfc2ae485aba326be83236440bc
Celia <celiasuarezcoronel@gmail.com>
2026-03-28 12:00:33 +0100

    fix: arreglo para pasar sonarqube

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3^f2dcx9gzk3s_e31(m8n!2!-4e^(#qi%la@@vvlpcvv!cez$$"
+SECRET_KEY = os.environ["SECRET_KEY_DJANGO"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 HOOK_DIR=".githooks"
 HOOK_FILE="$HOOK_DIR/commit-msg"
 
-if [ ! -f "$HOOK_FILE" ]; then
+if [[ ! -f "$HOOK_FILE" ]]; then
   echo "No hook found at $HOOK_FILE"
   exit 1
 fi
